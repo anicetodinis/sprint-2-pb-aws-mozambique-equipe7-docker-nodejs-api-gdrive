73081193deb2d05690215a52729d40cee5b85c75
Aniceto Dinis <anicetodinis.ad@gmail.com>
2024-02-27 21:08:27 +0200

    feat(public/src/script.js): selecionar ficheiros

--- public/src/script.js
+++ public/src/script.js
@@ -1,2 +1,17 @@
+var fileInput = document.getElementById('fileInput');
+var fileInputLabel = document.getElementById('fileInputLabel');
 
+fileInput.addEventListener('change', function () {
+    if (fileInput.files && fileInput.files.length > 0) {
+        var fileName = '';
+        if (fileInput.files.length === 1) {
+            fileName = fileInput.files[0].name;
+        } else {
+            fileName = fileInput.files.length + ' fotografias selecionadas';
+        }
+        fileInputLabel.textContent = fileName;
+    } else {
+        fileInputLabel.textContent = 'Selecionar Ficheiro uma ou mais fotografias';
+    }
+});
 
